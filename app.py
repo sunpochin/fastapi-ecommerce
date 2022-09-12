8916a07150cde4c6b4dd999ce4451fbf79769568
pac <sunpochin@gmail.com>
2022-09-13 02:43:10 +0800

    add main.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Welcome to the API!"}
