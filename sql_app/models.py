a593f09d73489b21e7c5add2164b29cdfdc288ae
pac <sunpochin@gmail.com>
2022-09-13 18:35:12 +0800

    try to fix render io deployment

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,7 +1,17 @@
+from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
-
 from .database import Base
+
+class Song(Base):
+    __tablename__ = 'songs'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(128), nullable=False)
+    artist = Column(String(128), nullable=False)
+    release_date = Column(Date, nullable=False)
+
+
 
 
 class User(Base):
