e192ce5d15fba4df12b0eb95a302088757489358
pac <sunpochin@gmail.com>
2022-09-19 21:33:07 +0800

    SQLALCHEMY_DATABASE_URL

--- sql_app/database.py
+++ sql_app/database.py
@@ -6,13 +6,17 @@ import os  # provides ways to access the Operating System and allows us to read 
 
 load_dotenv()
 
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = os.getenv("DBHOST")
-
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 engine = create_engine(
-#    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
+# SQLALCHEMY_DATABASE_URL = os.getenv("DBHOST")
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+#     SQLALCHEMY_DATABASE_URL
+# )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
